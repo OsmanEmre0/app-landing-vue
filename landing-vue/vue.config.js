69e3c8bb31a645ddedda25d20511d1467c421336
Osman Emre Sarıahmetoğlu <osariahnetoglu@gmail.com>
2024-12-21 00:17:36 +0300

    Netlify

--- landing-vue/vue.config.js
+++ landing-vue/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   devServer: {
     port: 8080,
     open: true,
